4d7680a6350addfae866568ff392e086bf315147
Teo <teodor@dahlknutsen.no>
2019-07-04 11:26:23 +0200

    Render pixel for pixel to surface

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,93 @@ extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::rect::Point;
 use std::time::Duration;
 
-#[derive(Copy, Clone)]
-struct ColoredPoint {
-    color: Color,
-    point: Point,
+pub mod render {
+    use sdl2::video::WindowSurfaceRef;
+
+    fn cap(c: f32, min: f32, max: f32) -> f32 {
+        if c > max {
+            return max;
+        }
+        if c < min {
+            return min;
+        }
+
+        c
+    }
+
+    pub fn color(r: f32, g: f32, b: f32) -> u32 {
+        let r = cap(r, 0., 1.);
+        let g = cap(g, 0., 1.);
+        let b = cap(b, 0., 1.);
+
+        let mut color: u32 = (r * 255.) as u32;
+        color *= 256;
+        color += (g * 255.) as u32;
+        color *= 256;
+        color += (b * 255.) as u32;
+
+        color
+    }
+
+    pub fn set_pixel(pixels: &mut Vec<u32>, w: usize, h: usize, x: usize, y: usize, color: u32) -> () {
+        if x >= w || y > h {
+            return ();
+        }
+        pixels[x + y * w] = color;
+
+        ()
+    }
+
+    pub fn copy_to_surface(surface: &WindowSurfaceRef, pixels: &mut Vec<u32>) -> () {
+        let (w, h) = surface.size();
+        unsafe {
+            let raw_surface = *surface.raw();
+            raw_surface.pixels
+                .copy_from(pixels.as_mut_ptr() as *mut std::ffi::c_void, w as usize * h as usize * 4);
+        }
+        ()
+    }
+
+    pub fn generate_pixels(w: usize, h: usize) -> Vec<u32> {
+        let mut pixles: Vec<u32> = Vec::with_capacity(w * h);
+        for _ in 0..(w * h) {
+            pixles.push(0);
+        }
+
+        pixles
+    }
+
+    pub fn clear(pixels: &mut Vec<u32>, w: usize, h: usize) -> () {
+        for x in  0..w {
+            for y in 0..h {
+                pixels[x + w * y] = 0;
+            }
+        }
+    }
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
+    let w: usize = 640;
+    let h: usize = 480;
+
     let window = video_subsystem
-        .window("SDL2", 640, 480)
+        .window("SDL2", w as u32, h as u32)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
 
-    let mut canvas = window
-        .into_canvas()
-        .accelerated()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    canvas.set_draw_color(sdl2::pixels::Color::RGBA(0, 0, 0, 255));
-
-    // animation sheet and extras are available from
-    // https://opengameart.org/content/a-platformer-in-the-forest
-    //let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/characters.bmp"))?;
-    //
     let mut event_pump = sdl_context.event_pump()?;
 
-    let mut points = [ColoredPoint { color: Color::RGBA(255, 0, 255, 255), point: Point::new(0, 0) }; 640 * 480];
-    for x in 0..640 {
-        for y in 0..480 {
-            points[x + y * 640].point.x = x as i32;
-            points[x + y * 640].point.y = y as i32;
+    let mut pixles = render::generate_pixels(w, h);
+
+    for x in 0..w {
+        for y in 0..h {
+            render::set_pixel(&mut pixles, w, h, x, y, render::color(1., 0., 1.));
         }
     }
 
@@ -59,13 +107,10 @@ fn main() -> Result<(), String> {
             }
         }
 
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
+        let surface = window.surface(&event_pump)?;
 
-        canvas.set_draw_color(Color::RGB(255, 0, 255));
-        canvas.draw_points(&points[..]);
-
-        canvas.present();
+        render::copy_to_surface(&surface, &mut pixles);
+        surface.update_window().unwrap();
 
         std::thread::sleep(Duration::from_millis(100));
     }
